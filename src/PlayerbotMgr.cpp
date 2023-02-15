e25ac6ebef6363182ca7d85b8e8974258d0684b9
Daniel S. Reichenbach <daniel@kogito.network>
2023-02-15 12:54:33 +0100

    fix: ensure we only act if playerbots are enabled

--- src/PlayerbotMgr.cpp
+++ src/PlayerbotMgr.cpp
@@ -1109,7 +1109,7 @@ void PlayerbotsMgr::RemovePlayerBotData(ObjectGuid const &guid)
 
 PlayerbotAI *PlayerbotsMgr::GetPlayerbotAI(Player *player)
 {
-    if (!player || (!player->IsInWorld() && !player->IsBeingTeleported()))
+    if (!(sPlayerbotAIConfig->enabled) || !player || (!player->IsInWorld() && !player->IsBeingTeleported()))
     {
         return nullptr;
     }
@@ -1125,7 +1125,7 @@ PlayerbotAI *PlayerbotsMgr::GetPlayerbotAI(Player *player)
 
 PlayerbotMgr *PlayerbotsMgr::GetPlayerbotMgr(Player *player)
 {
-    if (!player || (!player->IsInWorld() && !player->IsBeingTeleported()))
+    if (!(sPlayerbotAIConfig->enabled) || !player || (!player->IsInWorld() && !player->IsBeingTeleported()))
     {
         return nullptr;
     }
